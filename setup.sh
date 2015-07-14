03a390bbc639519d878c3decd436ccd52de82d74
sourceperl <loic.celine@free.fr>
2015-07-14 15:31:44 +0200

    add setup.sh to clone github repo

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/sourceperl/docker.mosquitto.git
+git clone https://github.com/sourceperl/docker.sigserver.git
+git clone https://github.com/sourceperl/docker.mqttwarn.git
